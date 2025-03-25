886c324b53738b4d63872074e4242623852225ed
Jay Patel <jaybaldha2931@gmail.com>
2025-03-25 09:18:37 +0530

    mm

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
 
 app.use("*", (req, res) => {
-  res.status(404).json({ message: "Invalid Request AS 2 master ok as" });
+  res.status(404).json({ message: "Invalid Request AS 2 master ok as mm" });
 });
 
 app.listen(port, () => {
